ce701f69e23a5c9190bc9842f5668bb6ef3758c4
Mattis Hallberg <skatemattis97@hotmail.com>
2026-02-24 18:39:02 +0100

    "" vercel bug

--- components/Disclaimer.js
+++ components/Disclaimer.js
@@ -36,7 +36,7 @@ const Disclaimer = () => {
             </p>
 
             <p className="text-base-content/80">
-              Istället för att lista "allt", lyfts enbart det fram som faktiskt
+              Istället för att lista allt, lyfts enbart det fram som faktiskt
               är relevant och intressant för dig, så att du sparar tid.
             </p>
           </div>
